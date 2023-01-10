5391b142334c163f2f26160868c8ced150390e54
Zhehao <x97589125@gmail.com>
2023-01-09 22:13:19 -0800

    week1

--- app/src/main/java/edu/northeastern/numad23sp_zhehaoxu/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_zhehaoxu/MainActivity.java
@@ -3,6 +3,9 @@ package edu.northeastern.numad23sp_zhehaoxu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button aboutMe=findViewById(R.id.About_me);
+        aboutMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Name: Zhehao Xu\nEmail: xu.zheh@northeastern.edu", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 }
