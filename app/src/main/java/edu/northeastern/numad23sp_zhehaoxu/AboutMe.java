e1043be262fab75195e1b6fa0aafba8e7cc1769d
Zhehao <x97589125@gmail.com>
2023-02-01 16:47:34 -0800

    Changed about_me from toast to activity

---
+++ app/src/main/java/edu/northeastern/numad23sp_zhehaoxu/AboutMe.java
@@ -0,0 +1,12 @@
+package edu.northeastern.numad23sp_zhehaoxu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutMe extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_me);
+    }
+}
--- app/src/main/java/edu/northeastern/numad23sp_zhehaoxu/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_zhehaoxu/MainActivity.java
@@ -4,9 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,8 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button aboutMe=findViewById(R.id.About_me);
-        String aboutMeText="Name: Zhehao Xu\nEmail: xu.zheh@northeastern.edu";
-        aboutMe.setOnClickListener(view -> Toast.makeText(MainActivity.this, aboutMeText, Toast.LENGTH_SHORT).show());
+        aboutMe.setOnClickListener(view -> startActivity(new Intent(MainActivity.this,AboutMe.class)));
         Button clickyButton = findViewById(R.id.clicky);
         clickyButton.setOnClickListener(view -> startActivity(new Intent(MainActivity.this,Clicky.class)));
     }
